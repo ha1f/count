8eb7f28eba9e20b15315b6da9fcf2f1f2041130b
ha1f <half.ldk.hiz@gmail.com>
2016-05-18 14:15:50 +0900

    fix moon

--- ha1fCount/ViewController/View/MoonView.swift
+++ ha1fCount/ViewController/View/MoonView.swift
@@ -11,12 +11,14 @@ import UIKit
 
 class MoonView: CounterNumberView {
     
-    static let COLOR_MOON = Color(UIColor.colorFromHex("000000", alpha: 1.0))
-    static let COLOR_MOON_DARK = Color(Constants.BACK_COLOR)
+    static let COLOR_MOON = Color(UIColor.colorFromHex("ffff00", alpha: 1.0))
+    static let COLOR_MOON_DARK = Shape().strokeColor!
     
     var moon: Arc!
     var darkMoon: Arc!
     var darkHalfMoon: Arc!
+    
+    let defaultCircleRadius: Double = 10
     
     var moonRadius: Double {
         return frame.width/4
@@ -25,17 +27,21 @@ class MoonView: CounterNumberView {
     override init(frame: Rect) {
         super.init(frame: frame)
         
+        let center = Point(self.bounds.width/2, self.bounds.height/2)
+        
+        self.backgroundColor = MoonView.COLOR_MOON_DARK
+        
         let radius = moonRadius
         
         // 右
-        moon = Arc(center: self.center, radius: radius, start: -M_PI/2, end: M_PI/2)
+        moon = Arc(center: center, radius: radius, start: -M_PI/2, end: M_PI/2)
         moon.fillColor = MoonView.COLOR_MOON
         moon.strokeColor = MoonView.COLOR_MOON
         moon.anchorPoint = Point(0.0, 0.5)
         self.add(moon)
         
         // 左
-        darkHalfMoon = Arc(center: self.center, radius: radius, start: M_PI/2, end: M_PI*3/2)
+        darkHalfMoon = Arc(center: center, radius: radius, start: M_PI/2, end: M_PI*3/2)
         darkHalfMoon.fillColor = MoonView.COLOR_MOON_DARK
         darkHalfMoon.strokeColor = MoonView.COLOR_MOON_DARK
         darkHalfMoon.anchorPoint = Point(1.0, 0.5)
@@ -54,6 +60,7 @@ class MoonView: CounterNumberView {
     
     override func updateView(newNumber: Int) {
         let radius = moonRadius
+        let center = Point(self.bounds.width/2, self.bounds.height/2)
         
         // なんとか倍率とかで計算できないか？(アニメーション)
         
@@ -82,15 +89,18 @@ class MoonView: CounterNumberView {
         let x = radius / 2 * (1.0 / cos(theta) + cos(theta))
         let a = asin(radius / x)
         
-        let newMoon = Arc(center: Point((self.center.x + radius * cos(theta) - x), self.center.y), radius: x, start: -a, end: a)
-        
+        let newMoon = Arc(center: Point((center.x + radius * cos(theta) - x), center.y), radius: x, start: -a, end: a)
+        /*if cos(theta) > 0 {
+            newMoon.anchorPoint = Point(-(x-radius*cos(theta))/(radius*cos(theta)), 0.5)
+        } else {
+            newMoon.anchorPoint = Point(1.0 + (x-radius*cos(theta))/(radius*cos(theta)), 0.5)
+        }
+        newMoon.center = Point((self.center.x + radius * cos(theta) - x), self.center.y)*/
         newMoon.fillColor = color
         newMoon.strokeColor = color
         
-        ShapeLayer.disableActions = true
-        self.darkMoon?.removeFromSuperview()
+        self.view.addSubview(newMoon.view)
+        self.darkMoon?.view.removeFromSuperview()
         self.darkMoon = newMoon
-        self.add(newMoon)
-        ShapeLayer.disableActions = false
     }
 }
